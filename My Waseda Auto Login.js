0c747f43ce746a31185e281eb03701fcf0e329bf
aosa4054 <shoara0509@gmail.com>
2018-04-19 21:19:07 +0900

    Create My Waseda Auto Login.js

---
+++ My Waseda Auto Login.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         My Waseda Auto Login
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://iaidp.ia.waseda.jp/idp/profile/SAML2/Redirect/SSO?execution=*
+// @grant        none
+//@require       https://iaidp.ia.waseda.jp/idp/js/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    document.getElementById("j_username").value="君のログインId";
+    document.getElementById("j_password").value="君のパスワード";
+    document.getElementById("btn-save").click();
+})();
